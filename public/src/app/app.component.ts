bd6c7656b4441e48369490625e6a7f9e216bf757
Maiya Tracy <maiya.tracy@gmail.com>
2019-07-15 10:22:50 -0700

    rt continued

--- public/src/app/app.component.ts
+++ public/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HttpService } from './http.service';
 
 @Component({
@@ -6,7 +6,33 @@ import { HttpService } from './http.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'the universe';
-  constructor(private _httpService: HttpService){}
+  constructor(private _httpService: HttpService) { }
+  ngOnInit() {
+    this.getTasksFromService();
+  }
+  tasks = [];
+  getTasksFromService() {
+    this._httpService.getTasks().subscribe(data => {
+      console.log("Got our tasks!", data)
+      this.tasks = data.data;
+    })
+  }
 }
+
+
+// num: number;
+// randNum: number;
+// str: string;
+// first_name: string;
+// snacks: string[];
+// loggedIn: boolean;
+
+
+//   this.snacks = ["vanilla latte with skim milk", "brushed suede", "cookie"];
+//   this.loggedIn = true;
+//   this.num = 7;
+//   this.randNum = Math.floor((Math.random() * 2) + 1);
+//   this.str = 'Hello Angular Developer!';
+//   this.first_name = 'Alpha';
--- public/src/app/http.service.ts
+++ public/src/app/http.service.ts
@@ -9,52 +9,58 @@ export class HttpService {
 
   constructor(private _http: HttpClient) {
     // this.getTasks();
-    this.getPokemon();
-    this.getAbilities();
-    this.getAbility1();
-    this.getAbility2();
-    this.getAbility1Users();
-    this.getAbility2Users();
+
   }
   getTasks() {
     // our http response is an Observable, store it in a variable
-    let tempObservable = this._http.get('/tasks');
+    // let tempObservable = this._http.get('/tasks');
     // subscribe to the Observable and provide the code we would like to do with our data from the response
-    tempObservable.subscribe(data => console.log("Got our tasks!", data));
+    // tempObservable.subscribe(data => console.log("Got our tasks!", data));
+    return this._http.get('/tasks');
   }
-  getPokemon() {
-    let pokemon = this._http.get('https://pokeapi.co/api/v2/pokemon/38/');
-    pokemon.subscribe(data => console.log(`Got ${data.name}!`, data));
-  }
-  getAbilities() {
-    let pokemon = this._http.get('https://pokeapi.co/api/v2/pokemon/38/');
-    pokemon.subscribe(data => console.log(`Got ${data.name}'s abilities: ${data.abilities[0].ability.name} and ${data.abilities[1].ability.name}`));
-  }
-  getAbility1() {
-    let ability1 = this._http.get('https://pokeapi.co/api/v2/ability/70/');
-    ability1.subscribe(data => console.log(`Got the ${data.name} ability: ${data.pokemon.length}`));
-  }
-  getAbility2() {
-    let ability2 = this._http.get('https://pokeapi.co/api/v2/ability/18/');
-    ability2.subscribe(data => console.log(`Got the ${data.name} ability: ${data.pokemon.length}`));
-  }
-  getAbility1Users() {
-    let ability1users = this._http.get('https://pokeapi.co/api/v2/ability/70/');
-    ability1users.subscribe(data => {
-      console.log(`Here are the pokemon with ability ${data.name}`)
-      for (let x of data.pokemon) {
-        console.log(`${x.pokemon.name}`);
-      }
-    });
-  }
-  getAbility2Users() {
-    let ability2users = this._http.get('https://pokeapi.co/api/v2/ability/18/');
-    ability2users.subscribe(data => {
-      console.log(`Here are the pokemon with ability ${data.name}`)
-      for (let x of data.pokemon) {
-        console.log(`${x.pokemon.name}`);
-      }
-    });
-  }
-
 }
+
+
+  // this.getPokemon();
+  // this.getAbilities();
+  // this.getAbility1();
+  // this.getAbility2();
+  // this.getAbility1Users();
+  // this.getAbility2Users();
+
+
+
+  // getPokemon() {
+  //   let pokemon = this._http.get('https://pokeapi.co/api/v2/pokemon/38/');
+  //   pokemon.subscribe(data => console.log(`Got ${data.name}!`, data));
+  // }
+  // getAbilities() {
+  //   let pokemon = this._http.get('https://pokeapi.co/api/v2/pokemon/38/');
+  //   pokemon.subscribe(data => console.log(`Got ${data.name}'s abilities: ${data.abilities[0].ability.name} and ${data.abilities[1].ability.name}`));
+  // }
+  // getAbility1() {
+  //   let ability1 = this._http.get('https://pokeapi.co/api/v2/ability/70/');
+  //   ability1.subscribe(data => console.log(`Got the ${data.name} ability: ${data.pokemon.length}`));
+  // }
+  // getAbility2() {
+  //   let ability2 = this._http.get('https://pokeapi.co/api/v2/ability/18/');
+  //   ability2.subscribe(data => console.log(`Got the ${data.name} ability: ${data.pokemon.length}`));
+  // }
+  // getAbility1Users() {
+  //   let ability1users = this._http.get('https://pokeapi.co/api/v2/ability/70/');
+  //   ability1users.subscribe(data => {
+  //     console.log(`Here are the pokemon with ability ${data.name}`)
+  //     for (let x of data.pokemon) {
+  //       console.log(`${x.pokemon.name}`);
+  //     }
+  //   });
+  // }
+  // getAbility2Users() {
+  //   let ability2users = this._http.get('https://pokeapi.co/api/v2/ability/18/');
+  //   ability2users.subscribe(data => {
+  //     console.log(`Here are the pokemon with ability ${data.name}`)
+  //     for (let x of data.pokemon) {
+  //       console.log(`${x.pokemon.name}`);
+  //     }
+  //   });
+  // }
